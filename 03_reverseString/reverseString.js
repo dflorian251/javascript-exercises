1ff60fe8759f3eb326d5ab1a24c2dc68a06d6d61
dflorian251 <inf2021044@ionio.gr>
2023-10-02 09:12:09 +0300

    Solution of the third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let finalString = '' ;
+    let i = string.length - 1;
+    while (i>=0) {
+        finalString = finalString.concat(string[i]) ; 
+        i -- ;
+    }
+    return finalString ; 
 };
 
 // Do not edit below this line
