b2c2653a005247ee16d4936a2aff53e8d1dfdecb
dflorian251 <inf2021044@ionio.gr>
2023-10-03 09:47:22 +0300

    Very besicly structure of the final solution
    Not all tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...shouldRemoved) { // ...shouldRemoved is array 
+    let finalArray = [];
+    finalArray = finalArray.concat(array);
+    for (let i = 0; i<array.length; i++) {
+        let j = 0 ;
+        while (j<shouldRemoved.length) {
+            if (array[i] == shouldRemoved[j]) {
+                finalArray.splice(i,1);
+            }
+            j++ ;
+        } 
+        //finalArray = finalArray.concat(element) ;
+    }
+    console.log(finalArray);
+    return finalArray ; 
 };
 
 // Do not edit below this line
