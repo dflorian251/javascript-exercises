5ee3d392b98c71f44b762da1a1edc82f5c61ffe3
dflorian251 <inf2021044@ionio.gr>
2023-10-04 09:56:25 +0300

    Solution of the 7th exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  celsius = ((f - 32) * (5/9));
+  return Math.ceil(celsius * 10) / 10 ;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  fahrenheit = (c * (9/5) + 32) ;
+  return Math.ceil(fahrenheit * 10) / 10 ;
 };
 
 // Do not edit below this line
